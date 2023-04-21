96fd806be9c44b7a14e23b67d65d29d8955c2b8e
Yana <yanalunina96@gmail.com>
2023-04-21 17:45:28 +0300

    .

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -14,15 +14,40 @@ class Serchbar extends Component {
     request: '',
   };
 
-        <SearchFormInput
-          type="text"
-          autocomplete="off"
-          placeholder="Search images and photos"
-        />
-      </SearchForm>
-    </Header>
-  );
-};
+  handelCategotyChange = e => {
+    this.setState({ request: e.currentTarget.value.toLowerCase() });
+  };
+
+  handelSubmit = e => {
+    e.preventDefault();
+
+    if (this.state.request.trim() === '') {
+      return Notiflix.Notify.failure('You have entered a category name');
+    }
+
+    this.props.onSubmit(this.state.request);
+    this.setState({ request: '' });
+  };
+  render() {
+    const { request } = this.state;
+    return (
+      <Header>
+        <SearchForm onSubmit={this.handelSubmit}>
+          <SearchFormInput
+            onChange={this.handelCategotyChange}
+            value={request}
+            type="text"
+            autocomplete="off"
+            placeholder="Search images and photos"
+          />
+          <SearchFormButton type="submit">
+            <SerchIcon width="30px" height="30px" />
+          </SearchFormButton>
+        </SearchForm>
+      </Header>
+    );
+  }
+}
 
 // Serchbar.propTypes = {
 //   onSubmit: PropTypes.func.isRequired,
